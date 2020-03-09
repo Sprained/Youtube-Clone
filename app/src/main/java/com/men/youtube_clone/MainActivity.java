02474814d787a423702c1066967787c2caa1d202
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-03-09 10:26:10 -0300

    Libs, Permissões, API KEY
    Configuração da libs e api key do youtube, e configuração de permissão de acesso a internet do usuario

---
+++ app/src/main/java/com/men/youtube_clone/MainActivity.java
@@ -0,0 +1,16 @@
+package com.men.youtube_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String YOUTUBE_KEY = "****";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
